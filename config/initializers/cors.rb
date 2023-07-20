e62088a9f13ec0d6d7c7f30324714620586f93b5
johnfinley <johnfinleybremerton@gmail.com>
2023-07-20 17:08:09 -0600

    change name of cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://outofboundsdics.netlify.app"
+    origins "https://outofboundsdiscs.netlify.app"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
